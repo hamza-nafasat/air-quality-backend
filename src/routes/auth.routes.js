807f087a20435802b769fe1eb8c4a6f3e4a94616
hamza-nafasat <hamzanafasat4155@gmail.com>
2024-10-04 17:37:44 +0500

    .

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { getMyProfile, login, logout, register } from "../controllers/auth.controller.js";
 import { isAuthenticated } from "../middlewares/authMiddleware.js";
-import { singleUpload } from "../middlewares/multer.js";
 
 const app = express();
 
